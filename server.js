8db096596b2394649ce0b9cd33f952400dd1a4a0
Matthew Boyea <yourzombiemop@gmail.com>
2022-12-20 15:36:02 -0600

    implemented home page resolution for local server

--- server.js
+++ server.js
@@ -3,6 +3,17 @@ const fs = require('fs');
 
 // basic server which doesn't cache files
 http.createServer((req, res) => {
+  if (req.url == '/') {
+    fs.readFile(__dirname + '/index.html', (err, data) => {
+      if (err) {
+        res.writeHead(404);
+        res.end(JSON.stringify(err));
+        return;
+      }
+      res.writeHead(200);
+      res.end(data);
+    })
+  }
   fs.readFile(__dirname + req.url, (err, data) => {
     if (err) {
       res.writeHead(404);
