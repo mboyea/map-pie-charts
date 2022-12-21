0a791cf42bcc0d578b6ac7aa6ccf140a5249e2d2
Matthew Boyea <yourzombiemop@gmail.com>
2022-12-21 10:38:34 -0600

    made width/height code-specificied and outlined future map obj properties

--- map-pie-chart.js
+++ map-pie-chart.js
@@ -1,15 +1,13 @@
 // The svg
-const svg = d3.select("svg#map"),
-  width = +svg.attr("width"),
-  height = +svg.attr("height");
+const svg = d3.select('svg#map');
+svg.attr('width', 1600);
+svg.attr('height', 900);
 
 // Map and projection
 const projection = d3.geoMercator()
 
-// Load external data and boot
-d3.json("data/usa-states.json").then( (data) => {
-  // // Filter data
-  // data.features = data.features.filter(d => {console.log(d.properties.name); return d.properties.name=="France"});
+// Load USA map
+d3.json("data/usa-states.json").then((data) => {
   // Draw the map
   svg.append("g")
     .selectAll("path")
@@ -19,3 +17,8 @@ d3.json("data/usa-states.json").then( (data) => {
     .attr("d", d3.geoPath().projection(projection))
     .style("stroke", "none");
 })
+
+// Map properties:
+// X zoomLevel
+// Svg usaMap (display in site)
+// Svg[] donutGraphs (display on top)
