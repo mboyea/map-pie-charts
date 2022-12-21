f150a31087b8f7ddaca69cc1878ad2c901eeae7e
Matthew Boyea <yourzombiemop@gmail.com>
2022-12-21 11:02:41 -0600

    implemented donut chart tutorial, made map svg target a div to inject into

--- map-pie-chart.js
+++ map-pie-chart.js
@@ -1,15 +1,8 @@
-// The svg
-const svg = d3.select('svg#map');
-svg.attr('width', 1600);
-svg.attr('height', 900);
-
-// Map and projection
+const mapContainer = d3.select('#map');
+const mapSvg = mapContainer.append("svg").attr('width', 800).attr('height', 450);
 const projection = d3.geoMercator()
-
-// Load USA map
 d3.json("data/usa-states.json").then((data) => {
-  // Draw the map
-  svg.append("g")
+  mapSvg.append("g")
     .selectAll("path")
     .data(data.features)
     .join("path")
@@ -17,6 +10,49 @@ d3.json("data/usa-states.json").then((data) => {
     .attr("d", d3.geoPath().projection(projection))
     .style("stroke", "none");
 })
+
+// set the dimensions and margins of the graph
+const width = 450,
+    height = 450,
+    margin = 40;
+
+// The radius of the pieplot is half the width or half the height (smallest one). I subtract a bit of margin.
+const radius = Math.min(width, height) / 2 - margin
+
+// append the svg object to the div called 'my_dataviz'
+const svg = d3.select("#donut-chart")
+  .append("svg")
+    .attr("width", width)
+    .attr("height", height)
+  .append("g")
+    .attr("transform", `translate(${width / 2},${height / 2})`);
+
+// Create dummy data
+const data = {a: 9, b: 20, c:30, d:8, e:12}
+
+// set the color scale
+const color = d3.scaleOrdinal()
+  .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56"])
+
+// Compute the position of each group on the pie:
+const pie = d3.pie()
+  .value(d=>d[1])
+
+const data_ready = pie(Object.entries(data))
+
+// Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
+svg
+  .selectAll('whatever')
+  .data(data_ready)
+  .join('path')
+  .attr('d', d3.arc()
+    .innerRadius(100)         // This is the size of the donut hole
+    .outerRadius(radius)
+  )
+  .attr('fill', d => color(d.data[0]))
+  .attr("stroke", "black")
+  .style("stroke-width", "2px")
+  .style("opacity", 0.7)
 
 // Map properties:
 // X zoomLevel
