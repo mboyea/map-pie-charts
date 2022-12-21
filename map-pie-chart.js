cc57709546d296dcc4963d923780159c541a9517
Matthew Boyea <yourzombiemop@gmail.com>
2022-12-21 09:30:00 -0600

    successfully rendered US states

--- map-pie-chart.js
+++ map-pie-chart.js
@@ -5,23 +5,17 @@ const svg = d3.select("svg#map"),
 
 // Map and projection
 const projection = d3.geoMercator()
-  .center([2, 47])                // GPS of location to zoom on
-  .scale(980)                       // This is like the zoom
-  .translate([ width/2, height/2 ])
 
 // Load external data and boot
-d3.json("data/usa-outline.json").then( function(data){
-
-// Filter data
-data.features = data.features.filter(d => {console.log(d.properties.name); return d.properties.name=="France"})
-// Draw the mapg.append("g")
-  .selectAll("path")
-  .data(data.features)
-  .join("path")
-  .attr("fill", "grey")
-  .attr(
-    "d",
-    d3.geoPath().projection(projection)
-  )
-  .style("stroke", "none")
+d3.json("data/usa-states.json").then( (data) => {
+  // // Filter data
+  // data.features = data.features.filter(d => {console.log(d.properties.name); return d.properties.name=="France"});
+  // Draw the map
+  svg.append("g")
+    .selectAll("path")
+    .data(data.features)
+    .join("path")
+    .attr("fill", "grey")
+    .attr("d", d3.geoPath().projection(projection))
+    .style("stroke", "none");
 })
